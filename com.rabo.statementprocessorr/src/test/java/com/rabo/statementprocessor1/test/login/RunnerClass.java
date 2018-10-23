fce3464b05e24bba7fe0a197724eda5180e354d5
vinay konda <kondavinay765@gmail.com>
2018-10-23 23:30:22 +0530

    commit

---
+++ com.rabo.statementprocessorr/src/test/java/com/rabo/statementprocessor1/test/login/RunnerClass.java
@@ -0,0 +1,12 @@
+package com.rabo.statementprocessor1.test.login;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="classpath:com\\rabo\\statementprocessor1\\test\\login\\RecordReferenceValidation.feature")
+public class RunnerClass {
+
+}
---
+++ com.rabo.statementprocessorr/src/test/java/com/rabo/statementprocessor1/test/login/StepDefination.java
@@ -0,0 +1,28 @@
+package com.rabo.statementprocessor1.test.login;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefination {
+	@Given("^list of records were passed as string array$")
+	public void list_of_records_were_passed_as_string_array() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^if the passed records consists duplicates remove them$")
+	public void if_the_passed_records_consists_duplicates_remove_them() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^valid records has to be printed$")
+	public void valid_records_has_to_be_printed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
